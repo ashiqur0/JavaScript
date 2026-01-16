b38a534356248346b23bda37b167e31b5ea02582
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-16 07:29:00 +0600

    Solve Two Sum Problem Using  Map In JavaScript

---
+++ Algorithm/TwoSum.js
@@ -0,0 +1,18 @@
+const twoSum = (arr, target) => {
+    const numbMap = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        const currentNumber = arr[i];
+        const complement = target - currentNumber;
+
+        if (numbMap.has(complement)) {
+            return [numbMap.get(complement), i];
+        }
+
+        numbMap.set(currentNumber, i);
+    }
+
+    return undefined;
+}
+
+console.log(twoSum([2, 11, 7, 15], 9));
