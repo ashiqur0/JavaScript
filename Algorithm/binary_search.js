643b9e5d318064784336ee42683543f43fb56555
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-18 07:11:13 +0600

    Add binary search algorithm implementation in JavaScript

---
+++ Algorithm/binary_search.js
@@ -0,0 +1,21 @@
+//? Binary Search Algorithm in JavaScript
+
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target not found
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)); // Output: 4
